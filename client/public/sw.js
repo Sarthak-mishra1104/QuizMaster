05319e9a1863308d9478f5ecf2755e3da71050a8
Sarthak <sarthakmishra.2431078@gmail.com>
2026-06-17 19:10:42 +0530

    fix broken service worker cache

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,29 +1,7 @@
-const CACHE_NAME = 'quizmaster-v1';
-const urlsToCache = [
-  '/',
-  '/index.html',
-  '/static/js/bundle.js',
-  '/manifest.json',
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('install', () => {
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
-    )
-  );
+  event.waitUntil(self.clients.claim());
 });
